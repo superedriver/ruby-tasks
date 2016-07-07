273e5128fc1d6874864a78898a0dd75872648166
superedriver <superedriver@gmail.com>
2016-07-07 15:04:22 +0300

    Divided GasStation into several classes

--- gasoline/gasoline.rb
+++ gasoline/gasoline.rb
@@ -1,90 +1,120 @@
 class GasStation
-  PRICES = {
-    "A95" => 1.2,
-    "A92" => 1.0,
-    "DIESEL" => 0.8
-  }
-  AVAILABLE_FUEL = PRICES.keys
   @@count = 0
-
-  def self.count
-    puts "We have #{@@count} gasoline stations"
-  end
 
   def initialize
     @@count += 1
+    @control = Control.new
+    @payment = Payment.new
+
     puts "Hi Dude! This is the first interacrive gasoline station!\n"
     puts "============================================"
     puts "Our prices:"
-    PRICES.each do |key, value|
+    @payment.fuel.each do |key, value|
       puts "#{key}: #{value}$"
     end
     puts "============================================"
     puts "Please write what do you need:"
-    puts "Available fuel: " + AVAILABLE_FUEL.join(", ")
+    puts "Available fuel: " + @payment.fuel.keys.join(", ")
     puts "give_me(fuel, liters)"
     puts "full_tank(fuel)"
   end
 
-  def give_me(fuel, liters)
-    return "We haven't got such fuel, sorry!" unless aviable_fuel(fuel)
-    return "Number of liters has to be more than 0!" if liters.to_i <= 0
+  def give_me(fuel=nil, liters=nil)
+    liters = liters.to_i
+    return "We haven't got such fuel, sorry!" unless has_this_fuel?(fuel)
+    return "Number of liters has to be more than 0!" if liters <= 0
 
-    payment(fuel, liters)
+    @payment.pay(fuel, liters)
 
-    refill(liters)
+    @control.refill(liters)
 
-    goodbye
+    @control.goodbye
   end
 
-  def full_tank(fuel)
-    return "We haven't got such fuel, sorry!" unless aviable_fuel(fuel)
+  def full_tank(fuel="")
+    return "We haven't got such fuel, sorry!" unless has_this_fuel?(fuel)
     liters = rand(1..10)
 
-    refill(liters)
+    @control.refill(liters)
 
-    payment(fuel, liters)
+    @payment.pay(fuel, liters)
 
-    goodbye
+    @control.goodbye
+  end
+
+  def count
+    puts "We have #{@@count} gasoline stations"
   end
 
   private
-    def payment(fuel, liters)
-      have_to_pay = (PRICES[fuel] * liters).round(2)
-      puts "You have to pay #{have_to_pay}$"
-      while (entered_money = (gets.chomp.to_f*100).floor/100.0) < have_to_pay
-        puts "Are you kidding me?"
-        puts "You have to pay #{have_to_pay}$"
-      end
-      if entered_money > have_to_pay
-        puts "Take your change: #{(entered_money - have_to_pay).round(2)}$!"
-      end
-      puts "Thanks a lot!"
-    end
-
-    def refill(liters)
-      puts "Start"
-      i = 0
-      while i <= liters
-        print i
-        break if i == liters
-        sleep 0.1
-        print  "."
-        sleep 0.1
-        print  "."
-        sleep 0.1
-        print  "."
-        sleep 0.1
-        i += 1
-      end
-      puts "\nFinish"
-    end
-
-    def aviable_fuel(fuel)
-      AVAILABLE_FUEL.include?(fuel)
-    end
-
-    def goodbye
-      puts "Have a nice day!"
+    def has_this_fuel?(fuel)
+      @payment.fuel.keys.include?(fuel.to_sym)
     end
 end
+
+
+class Control < GasStation
+  def initialize
+  end
+
+  def refill(liters)
+    puts "Start"
+    i = 0
+    while i <= liters
+      print i
+      break if i == liters
+      sleep 0.1
+      print  "."
+      sleep 0.1
+      print  "."
+      sleep 0.1
+      print  "."
+      sleep 0.1
+      i += 1
+    end
+    puts "\nFinish"
+  end
+
+  def goodbye
+    puts "Have a nice day!"
+  end
+end
+
+class Payment < GasStation
+  attr_reader :fuel
+
+  def initialize
+    @fuel = {
+      A95: 1.2,
+      A92: 1,
+      DIESEL: 0.8
+    }
+  end
+
+  def pay(fuel, liters)
+    have_to_pay = (@fuel[fuel.to_sym] * liters).round(2)
+    puts "You have to pay #{have_to_pay}$"
+    while (entered_money = (gets.chomp.to_f*100).floor/100.0) < have_to_pay
+      puts "Are you kidding me?"
+      puts "You have to pay #{have_to_pay}$"
+    end
+    if entered_money > have_to_pay
+      puts "Take your change: #{(entered_money - have_to_pay).round(2)}$!"
+    end
+    puts "Thanks a lot!"
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
